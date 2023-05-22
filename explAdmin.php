1661c43c6e2426052ad9927789b025bb9dee22b3
LouiseDecourselle <louise.decourselle@edu.ece.fr>
2023-05-22 13:27:34 +0200

    ajout

---
+++ explAdmin.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Mode d'emploi Admin</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="pied_de_page.css">
+        <link rel="stylesheet" type="text/css" href="css_Id_Ajout_Modif_Supp.css">
+    </head>
+<body>
+    Mode d’emploi :  <br>
+    <ul>
+        <li> Ajouter une école </li>
+        <li> Ajouter une promotion </li>
+        <li> Ajout matières </li>
+        <li> Ajout classes </li>
+        <li> Ajout professeurs </li>
+        <li> Ajout Etudiants </li>
+        <li> Affecter mat-prof </li>
+        <li> Affecter classe-prof </li>
+        <li> Affecter classe-matière </li>
+    </ul>
+
+    
+</body>
+</html>
+
+
+
